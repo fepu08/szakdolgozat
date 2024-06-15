b9acf61a5982ebf2e0dac7b6c28fe07c7af64941
fepu08 <ferencbesenyei@protonmail.com>
2024-06-15 16:55:29 +0200

    fixed backend baseURL

--- mvp/frontend-service/src/app/api/agent.ts
+++ mvp/frontend-service/src/app/api/agent.ts
@@ -7,7 +7,7 @@ import {
 import { MovieDTO } from '../../models/MovieModel';
 
 const agent = axios.create({
-  baseURL: `http://${import.meta.env.VITE_BACKEND_HOST}:${
+  baseURL: `${import.meta.env.VITE_BACKEND_HOST}:${
     import.meta.env.VITE_BACKEND_PORT
   }/api/v1`,
   withCredentials: true,
